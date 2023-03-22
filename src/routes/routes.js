dc424d49f4068e4dbb7425e0584cdd781697c294
khoand1 <khoand1@smartosc.com>
2023-03-22 15:15:12 +0700

    Update sidebar

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,6 +7,7 @@ import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
+import Live from '~/pages/Live';
 
 // public routes
 const publicRoutes = [
@@ -31,6 +32,10 @@ const publicRoutes = [
         path: config.routes.search,
         component: Search,
         layout: null,
+    },
+    {
+        path: config.routes.live,
+        component: Live,
     },
 ];
 
